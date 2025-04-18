23fa74556d015dd13f6a6999b7fc27624b770ee9
manbin <938770761@qq.com>
2025-04-18 09:43:47 +0800

    完成页面设计

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="home">
+    <a-space direction="vertical" size="large">
+      <a-typography-title :heading="4">欢迎来到广告平台</a-typography-title>
+      <a-card>
+        <a-space direction="vertical" size="large">
+          <a-typography-text>快速开始</a-typography-text>
+          <a-space>
+            <a-button type="primary" @click="$router.push('/buy')">购买广告</a-button>
+            <a-button @click="$router.push('/recharge')">广告充值</a-button>
+          </a-space>
+        </a-space>
+      </a-card>
+      
+      <a-row :gutter="16">
+        <a-col :span="8">
+          <a-card title="广告统计">
+            <a-statistic :value="0" title="今日展示" />
+          </a-card>
+        </a-col>
+        <a-col :span="8">
+          <a-card title="账户状态">
+            <a-statistic :value="0" title="账户余额" />
+          </a-card>
+        </a-col>
+        <a-col :span="8">
+          <a-card title="最近活动">
+            <a-empty description="暂无活动" />
+          </a-card>
+        </a-col>
+      </a-row>
+    </a-space>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.home {
+  max-width: 1200px;
+  margin: 0 auto;
+}
+</style> 
---
+++ frontend/src/views/RechargeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="recharge-view">
+    <a-typography-title :heading="4">广告充值</a-typography-title>
+    <a-card>
+      <a-empty description="广告充值功能开发中" />
+    </a-card>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.recharge-view {
+  max-width: 1200px;
+  margin: 0 auto;
+}
+</style> 
